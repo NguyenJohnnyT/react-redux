676f726dc6588cf1bad4b28c2c784adbfdc894d4
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-30 23:03:57 -0700

    add store, packages

---
+++ client/src/app/store.js
@@ -0,0 +1,9 @@
+// import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit';
+// import rootReducer from './reducer'
+
+export default configureStore({
+  reducer: {
+    
+  }
+})
