5ddbf5cee12f041b549c248d21ef180b633a01c3
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-30 23:15:56 -0700

    add slice files, import to store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,14 @@
 // import { createStore } from 'redux'
 import { configureStore } from '@reduxjs/toolkit';
 // import rootReducer from './reducer'
+import cartReducer from './cartSlice';
+import productReducer from './productSlice';
+import categoryReducer from './categorySlice';
 
 export default configureStore({
   reducer: {
-    
+    cart: cartReducer,
+    product: productReducer,
+    category: categoryReducer
   }
-})
+});
