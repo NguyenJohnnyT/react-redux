0e577de134a8fbb3d2279e34118d4c8f06f8e8a0
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-30 23:20:33 -0700

    updateProducts reducer

--- client/src/app/productSlice.js
+++ client/src/app/productSlice.js
@@ -1,3 +1,15 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
+export const productSlice = createSlice({
+  name: 'product',
+  initialState: {
+    value: [],
+  },
+  reducers: {
+    updateProducts(state,action) {
+      state.value = [...action.products];
+    }
+  }
+})
+
 export default productSlice.reducer;
